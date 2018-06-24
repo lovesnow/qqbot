d6cd28bd6a1e640c7cb72e55788a877d8ab93d5d
evilbeast <houshao55@gmail.com>
2018-06-24 19:49:17 +0800

    添加获取群组成员详细信息插件

---
+++ qqbot/plugins/getgroupfullinfo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+'''
+本插件启动时加载使用方法如下：
+
+    （1） 将本文件保存到插件目录中（如：C:\\Users\\xxx\\.qqbot-tmp\\plugins ）。
+
+    （2） 修改配置文件 v2.3.conf ，增加一个用户配置，增加以下内容：
+    
+             "myuser" : {
+                "plugins" : [
+                    "qqbot.plugins.schedrestart",
+                    "getgroupfullinfo"
+                ],
+                "pluginsConf" : {
+                    "qqbot.plugins.schedrestart" : "8:00",
+                    "getgroupfullinfo" : {
+                        "群名1" : "群号1",
+                        "群名2" : "群号2",
+                        "群名3" : "群号3",
+                    }
+                }
+            }
+
+         并修改其中的 "群名", "群号" .
+
+    （3） 启动 qqbot ： qqbot -u myuser
+
+'''
+
+def onUpdate(bot, tinfo):
+    # 当组更新完毕时,添加手动设置的QQ号
+    if tinfo == 'group':
+        group_manager = bot.conf.pluginsConf.get(__name__, {})
+        for group_name,group_qq in group_manager.iteritems():
+            groups = bot.List('group', group_name)
+            if len(groups) >= 1:
+                group = groups[0]  # 获取第一个
+                bot.contactdb.db.Modify('group', group, {'qq':group_qq})  # 更新qq号
